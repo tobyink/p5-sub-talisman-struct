90d43181f46ef074f9d2dd19734b2d9b774ad493
Toby Inkster <mail@tobyinkster.co.uk>
2014-09-10 22:59:44 +0100

    bump version; changelog

--- lib/Sub/Talisman/Struct.pm
+++ lib/Sub/Talisman/Struct.pm
@@ -6,7 +6,7 @@ use warnings;
 
 BEGIN {
 	$Sub::Talisman::Struct::AUTHORITY = 'cpan:TOBYINK';
-	$Sub::Talisman::Struct::VERSION   = '0.004';
+	$Sub::Talisman::Struct::VERSION   = '0.005';
 }
 
 use base qw( Sub::Talisman );
